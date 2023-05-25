9f59f7a0394ff653efa50d395b831f2c70503416
Joseph Martin <se.josephmartin@gmail.com>
2023-05-25 18:39:44 +0800

    add evenOrOdd.js

---
+++ 8kyu/evenOrOdd.js
@@ -0,0 +1,5 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    return number % 2 === 0 ? "Even"  : "Odd";
+  }
