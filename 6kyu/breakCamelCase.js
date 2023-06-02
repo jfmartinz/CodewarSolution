a619ee54f63073c9451a8bab9482ef840a6fb466
Joseph Martin <se.josephmartin@gmail.com>
2023-06-02 15:46:28 +0800

    add breakCamelCase.js

---
+++ 6kyu/breakCamelCase.js
@@ -0,0 +1,11 @@
+// Complete the solution so that the function will break up camel casing, 
+// using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+    return  string.split(/(?=[A-Z])/).join(" ");
+}
