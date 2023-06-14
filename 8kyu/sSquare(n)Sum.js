843c297544e379a17c45b3fd563519c36b6e5691
Joseph Martin <se.josephmartin@gmail.com>
2023-06-14 17:39:06 +0800

    Refractor sSquare(n).js

--- 8kyu/sSquare(n)Sum.js
+++ 8kyu/sSquare(n)Sum.js
@@ -4,10 +4,10 @@
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9
 
 function squareSum(numbers){
-    let sum = 0;
-    for(let i = 0; i < numbers.length; i++){
-      sum+=numbers[i] ** 2
-    }
-    return sum
-    }
+  let sum = 0;
+  numbers.forEach((number)=>{
+    sum+=number**2
+  })
+  return sum
+  }
     
