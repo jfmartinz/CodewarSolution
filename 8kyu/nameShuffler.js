039c7e8e655f4283199c430d40d90ac3928a760a
Joseph Martin <se.josephmartin@gmail.com>
2023-06-20 13:29:35 +0800

    add nameShuffler.js

---
+++ 8kyu/nameShuffler.js
@@ -0,0 +1,10 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
+  
