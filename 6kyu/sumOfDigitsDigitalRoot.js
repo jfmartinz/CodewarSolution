1bbeab1598356caf1762ef94d00cd6b78a5776ff
Joseph Martin <se.josephmartin@gmail.com>
2023-08-09 15:32:29 +0800

    add sumOfDigitsDigitalRoot.js

---
+++ 6kyu/sumOfDigitsDigitalRoot.js
@@ -0,0 +1,30 @@
+// Given n, take the sum of the digits of n. If that value has more than one digit,
+//  continue reducing in this way until a single-digit number is produced. 
+//  The input will be a non-negative integer.
+
+
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digitalRoot(n) {
+    if (n < 10) {
+      return n;
+    }
+  
+    let sum = 0;
+    while (n > 0) {
+      sum += n % 10;
+      n = Math.floor(n / 10);
+    }
+  
+    return digitalRoot(sum);
+  }
+  
+
+//   function digital_root(n) {
+//     return (n - 1) % 9 + 1;
+//   }
